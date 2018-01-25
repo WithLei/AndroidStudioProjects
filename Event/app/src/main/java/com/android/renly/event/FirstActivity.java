9a19d9c08b1cf794621571856b05c00ea7dbfad4
WithLei <syunki@yeah.net>
2018-01-25 17:00:28 +0800

    自定义控件

--- Event/app/src/main/java/com/android/renly/event/FirstActivity.java
+++ Event/app/src/main/java/com/android/renly/event/FirstActivity.java
@@ -1,35 +1,85 @@
 package com.android.renly.event;
 
+import android.annotation.SuppressLint;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-public class FirstActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity implements View.OnTouchListener{
+    private ImageView iv_first_pic;
+    private int lastX;
+    private int lastY;
+    private int maxX;
+    private int maxY;
+    private RelativeLayout parentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        iv_first_pic = findViewById(R.id.iv_first_pic);
+//        parentView = (RelativeLayout) iv_first_pic.getParent();
+        //设置监听
+        iv_first_pic.setOnTouchListener(this);
     }
 
-    public void onPicClick(View v){
-        Toast.makeText(this,"点击触发",Toast.LENGTH_SHORT).show();
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int eventX = (int) event.getRawX();
+        int eventY = (int) event.getRawY();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                //第一次记录lastX,lastY
+                if(maxY==0){
+                    parentView = (RelativeLayout) v.getParent();
+                    maxX = parentView.getRight();
+                    maxY = parentView.getBottom();
+                }
+                //Toast.makeText(this,maxX+"--"+maxY,Toast.LENGTH_SHORT).show();
+                lastX = eventX;
+                lastY = eventY;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                //计算事件的偏移
+                int dx = eventX - lastX;
+                int dy = eventY - lastY;
+                //根据事件的偏移来一栋imageView
+                int top,left,right,down;
+                top = iv_first_pic.getTop() + dy;
+                left = iv_first_pic.getLeft() + dx;
+                right = iv_first_pic.getRight() + dx;
+                down = iv_first_pic.getBottom() +dy;
+                if(top<0){
+                    down -= top;
+                    top = 0;
+                }
+                if(left<0){
+                    right -= left;
+                    left = 0;
+                }
+                if(right>maxX){
+                    left -= right - maxX;
+                    right = maxX;
+                }
+                if(down>maxY){
+                    top -= down - maxY;
+                    down = maxY;
+                }
+                iv_first_pic.layout(left,top,right,down);
+                lastX = eventX;
+                lastY = eventY;
+                break;
+            default:break;
+        }
+        return true;
     }
-
 }
--- Event/app/src/main/java/com/android/renly/event/MainActivity.java
+++ Event/app/src/main/java/com/android/renly/event/MainActivity.java
@@ -1,11 +1,23 @@
 package com.android.renly.event;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.app.Activity;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,5 +42,46 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    private boolean flag = false;
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg.what==1){
+                flag = false;
+            }
+        }
+    };
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        //监听Back键
+        if(event.getKeyCode()==KeyEvent.KEYCODE_BACK){
+            if (flag){
+                finish();
+            }
+            Toast.makeText(MainActivity.this,"再按一次退出", Toast.LENGTH_SHORT).show();
+            flag = true;
+            handler.sendEmptyMessageDelayed(1,2000);
+            return true;
+        }
+        return super.onKeyUp(keyCode, event);
+    }
+
+    public void onExitClick(View v){
+        //显示确定的dialog
+        new AlertDialog.Builder(this)
+                .setMessage("你确定退出吗")
+                .setPositiveButton("残忍的退出", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //退出
+                        finish();
+                    }
+                })
+                .setNegativeButton("再看看",null)
+                .show();
     }
 }
