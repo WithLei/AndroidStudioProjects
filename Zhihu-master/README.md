1e341d875218a964a1cc6b594c720c67c4727e2c
WithLei <syunki@yeah.net>
2018-01-28 17:35:10 +0800

    Tranning

---
+++ Zhihu-master/README.md
@@ -0,0 +1,22 @@
+# 知乎日报 简易版
+![image](https://raw.github.com/Kay-Wu/Zhihu/master/demo.gif)
+
+## Features
+* 下拉刷新(SwipeRefreshLayout)
+* 内容阅读(WebView以及相应CSS的应用)
+* listview使用卡片式UI(layer-list)
+* 收藏功能
+
+## Dependencies
+* gson-2.3.1
+* universal-image-loader-1.9.3
+
+## Thanks to
+* [ZhihuPaper](https://github.com/cundong/ZhihuPaper) css以及webview的使用
+* [ZhihuDailyPurify](https://github.com/izzyleung/ZhihuDailyPurify) 知乎API分析
+
+## APK
+[下载](https://raw.githubusercontent.com/KayWu/Zhihu/master/app-release.apk)
+
+##License
+GPL v2.0
---
+++ Zhihu-master/app/src/main/java/com/kay/zhihu/task/LoadNewsTask.java
@@ -0,0 +1,57 @@
+package com.kay.zhihu.task;
+
+import android.os.AsyncTask;
+
+import com.kay.zhihu.entity.News;
+import com.kay.zhihu.http.Http;
+import com.kay.zhihu.http.JsonHelper;
+import com.kay.zhihu.adapter.NewsAdapter;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by mac on 15-2-3.
+ */
+public class LoadNewsTask extends AsyncTask<Void, Void, List<News>> {
+    private NewsAdapter adapter;
+    private onFinishListener listener;
+
+    public LoadNewsTask(NewsAdapter adapter) {
+        super();
+        this.adapter = adapter;
+    }
+
+    public LoadNewsTask(NewsAdapter adapter, onFinishListener listener) {
+        super();
+        this.adapter = adapter;
+        this.listener = listener;
+    }
+
+    @Override
+    protected List<News> doInBackground(Void... params) {
+        List<News> newsList = null;
+        try {
+            newsList = JsonHelper.parseJsonToList(Http.getLastNewsList());
+        } catch (IOException | JSONException e) {
+
+        } finally {
+            return newsList;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(List<News> newsList) {
+        adapter.refreshNewsList(newsList);
+        if (listener != null) {
+            listener.afterTaskFinish();
+        }
+
+    }
+
+    public interface onFinishListener {
+        public void afterTaskFinish();
+    }
+}
