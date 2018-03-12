5749aeb63ccb127f1ab99f47b7d7ef0be8017c5c
WithLei <syunki@yeah.net>
2018-03-12 20:19:25 +0800

    volley

--- volley/app/src/main/java/com/android/renly/volley/MainActivity.java
+++ volley/app/src/main/java/com/android/renly/volley/MainActivity.java
@@ -1,5 +1,6 @@
 package com.android.renly.volley;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,11 +9,18 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -67,6 +75,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     }
                 }, new Response.ErrorListener() {
                     @Override
+
+
                     public void onErrorResponse(VolleyError error) {
                         tv_volly_text.setText("数据出错");
                     }
@@ -75,10 +85,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 requestQueue.add(stringRequest);
                 break;
             case R.id.btn_volly_post:
-                Toast.makeText(MainActivity.this,"post",Toast.LENGTH_LONG).show();
+                //创建一个请求队列
+                RequestQueue requestQueue1 = Volley.newRequestQueue(this);
+                //创建一个post请求
+                String url1 = "http://api.m.mtime.cn/PageSubArea/TrailerList.api";
+                tv_volly_text.setVisibility(View.VISIBLE);
+                StringRequest stringRequest1 = new StringRequest(Request.Method.POST, url1, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        tv_volly_text.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        tv_volly_text.setText("请求失败"+error);
+                    }
+                }){
+
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String,String> map = new HashMap<String,String>();
+                        return map;
+                    }
+                };
+                //将Post请求加入队列
+                requestQueue1.add(stringRequest1);
                 break;
             case R.id.btn_volly_image:
-                Toast.makeText(MainActivity.this,"image",Toast.LENGTH_LONG).show();
+                //imagerequest加载数据
+                tv_volly_text.setVisibility(View.GONE);
+                iv_volly_image.setVisibility(View.VISIBLE);
+                //创建一个请求队列
+                RequestQueue requestQueue2 = Volley.newRequestQueue(this);
+                //创建一个imagerequest请求
+                String url2 = "https://avatars2.githubusercontent.com/u/24489991?s=460&v=4";
+                ImageRequest imageRequest = new ImageRequest(url2, new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        iv_volly_image.setImageBitmap(response);
+                    }
+                }, 0, 0, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        iv_volly_image.setImageResource(R.drawable.doge);
+                    }
+                });
+
+                //将imagerequest请求加入队列
+                requestQueue2.add(imageRequest);
                 break;
             case R.id.btn_volly_imageloader:
                 Toast.makeText(MainActivity.this,"imageloader",Toast.LENGTH_LONG).show();
